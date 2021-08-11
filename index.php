0445035e7b30d587be6595c262234c36f6fdfb02
ronaldolsilva <ronaldo.lsilva@outlook.com>
2021-08-11 19:13:54 -0300

    Classe Admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -21,6 +22,12 @@ $app->get('/', function() {
 
 });
 
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
+    $page->setTpl("index");
+
+});
 $app->run();
 
  ?>
